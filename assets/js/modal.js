7102a301245cd2beac8f2bd31e3680cfb774e195
Robert Warneke <hello@robertwarneke.com>
2023-05-14 00:02:48 -0500

    commit

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,17 +1,19 @@
-var modal = document.getElementById("modal");
-var btn = document.getElementById("modal-btn");
-var span = document.getElementsByClassName("close")[0];
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.getElementById("modal");
+    const btn = document.getElementById("modal-btn");
+    const span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-	modal.style.display = "block";
-}
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
 
-span.onclick = function() {
-	modal.style.display = "none";
-}
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 
-window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-	}
-}
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
